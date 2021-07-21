fdec2603943e96d949799c6c820e7f604fded89a
guanzhen <guanzhen@inke.cn>
2021-07-21 15:41:26 +0800

    创建项目

---
+++ app/src/main/java/com/inke/childstudy/MainActivity.java
@@ -0,0 +1,17 @@
+package com.inke.childstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 主页面
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
