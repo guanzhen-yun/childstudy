4a620fa6504f415ddeca0d74d348feb96af45656
guanzhen <guanzhen@inke.cn>
2021-08-30 10:33:47 +0800

    添加热更新

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -215,3 +215,18 @@
 -keep class dialog.Rotate3dAnimation{
     public <init>(...);
 }
+
+#基线包使用，生成mapping.txt
+-printmapping mapping.txt
+#生成的mapping.txt在app/build/outputs/mapping/release路径下，移动到/app路径下
+#修复后的项目使用，保证混淆结果一致
+#-applymapping mapping.txt
+#hotfix
+#-keep class com.taobao.sophix.**{*;}
+#-keep class com.ta.utdid2.device.**{*;}
+##防止inline
+#-dontoptimize
+#-keepclassmembers class com.inke.childstudy.App {
+#    public <init>();
+#}
+#-keep class com.inke.childstudy.SophixStubApplication$RealApplicationStub
--- app/src/main/java/com/inke/childstudy/App.java
+++ app/src/main/java/com/inke/childstudy/App.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.inke.childstudy.utils.SharedPrefUtils;
 import com.inke.childstudy.utils.ToastUtils;
+import com.taobao.sophix.SophixManager;
 import com.ziroom.net.LogUtils;
 
 import cn.bmob.v3.Bmob;
@@ -18,5 +19,8 @@ public class App extends Application {
         Bmob.initialize(this, "2b7ef1eda2b02905fa9334a15b60492a");
         SharedPrefUtils.getInstance().init(this);
         LogUtils.debug = BuildConfig.DEBUG;
+        // queryAndLoadNewPatch为拉取控制台补丁
+        //不可放在attachBaseContext 中，否则无网络权限，建议放在主进程任意时刻，如Application的onCreate中
+        SophixManager.getInstance().queryAndLoadNewPatch();
     }
 }
