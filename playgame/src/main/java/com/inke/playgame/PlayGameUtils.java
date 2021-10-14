a7ba110e062dd5809825b182c5f1e502ec77e4c1
guanzhen <739144003@qq.com>
2021-10-14 09:50:04 +0800

    创建maven仓库说明

--- playgame/src/main/java/com/inke/playgame/PlayGameUtils.java
+++ playgame/src/main/java/com/inke/playgame/PlayGameUtils.java
@@ -3,7 +3,11 @@ package com.inke.playgame;
 import android.content.Context;
 import android.widget.Toast;
 
+/**
+ * https://blog.csdn.net/ocean20/article/details/108093223
+ */
 public class PlayGameUtils {
+    //nexus-3.34.0-01/bin download目录下 ./nexus start  http://localhost:8081
     public static void playGame(Context context, String playGame){
         Toast.makeText(context, "玩" + playGame, Toast.LENGTH_SHORT).show();
     }
